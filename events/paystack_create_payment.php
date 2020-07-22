388eb122be747cc7ecb8035884beaafdb786af2e
emeka <emeka@kali.kali>
2020-07-22 17:24:38 +0100

    done with the task

--- events/paystack_create_payment.php
+++ events/paystack_create_payment.php
@@ -18,7 +18,7 @@ $id_encrypted = $db_handle->sanitizePost($_GET['xxid']);
 $id_encrypted = decrypt(str_replace(" ", "+", $id_encrypted));
 echo $ssid = preg_replace("/[^A-Za-z0-9 ]/", '', $id_encrypted);
 
-$customer_email = $_SESSION['couprov_email'];
+$customer_email = $_SESSION['event_prov_email'];
 
 //create paystack lib object
 $paystack_lib_object = \MAbiola\Paystack\Paystack::make('sk_test_2183668043518716ea1276c2fded49d47a8c17cf');
@@ -29,7 +29,7 @@ $cart_amount = $event_prov_object->get_cart_total($ssid);
 $currency = $event_prov_object->get_cart_info($ssid)[0]['plan_currency'];
 $currency_info = $zenta_operation->get_currency_info($currency);
 $currency_rate = $currency_info['currency_rate'];
-$cart_amount = $cart_amount*$currency_rate*360*100; //make it in kobo
+$cart_amount = $cart_amount * $currency_rate * 360 * 100; //make it in kobo
 //create transaction
 try {
     $authorization = $paystack_lib_object->startOneTimeTransaction($cart_amount, $customer_email);
@@ -38,5 +38,5 @@ try {
     header('Location: ' . $authorization['authorization_url']);
 } catch (Exception $e) {
     header("Location: paystack_error.php?error={$e->getMessage()}");
-	//echo $e->getMessage();
+    //echo $e->getMessage();
 }
