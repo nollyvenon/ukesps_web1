d2ab645ae6d5285ccd5750507d3b7f595b5d3b71
emeka <emeka@kali.kali>
2020-08-12 11:54:51 +0100

    cv upload done

--- job_sector.php
+++ job_sector.php
@@ -1,51 +1,58 @@
 <?php
 include_once("z_db.php");
-require_once(LIB_PATH.DS."class_recruiter.php");
+require_once(LIB_PATH . DS . "class_recruiter.php");
 $recruit_object = new RecruitUser();
 $sector_id = $_GET['sid'];
-if(isset($_POST['search_text']) && strlen($_POST['search_text']) > 3) {
-    $search_text = $_POST['search_text'];
+if (isset($_POST['search_text']) && strlen($_POST['search_text']) > 3) {
+	$search_text = $_POST['search_text'];
 	$query = "SELECT jbv.*, jbc.sector_id, jbc.sector_name, jbl.location_name FROM jobs jbv
         INNER JOIN job_sectors jbc ON jbv.job_sector=jbc.sector_id
 		INNER JOIN job_locations jbl ON jbv.job_location=jbl.location_id
         WHERE (jbc.sector_name LIKE '%$search_text%' OR jbv.job_title LIKE '%$search_text%') AND (jbc.sector_id='sector_id' OR jbc.sector_name='$sector_id') order by jbv.jobs_id DESC ";
-    
 } else {
 	$query = "SELECT jbv.*, jbc.sector_id, jbc.sector_name, jbl.location_name FROM jobs jbv
         INNER JOIN job_sectors jbc ON jbv.job_sector=jbc.sector_id
  		INNER JOIN job_locations jbl ON jbv.job_location=jbl.location_id
-       WHERE (jbc.sector_id='$sector_id' OR jbc.sector_name='$sector_id') order by jbv.jobs_id DESC "; 
+       WHERE (jbc.sector_id='$sector_id' OR jbc.sector_name='$sector_id') order by jbv.jobs_id DESC ";
 }
 $numrows = $db_handle->numRows($query);
 
 // For search, make rows per page equal total rows found, meaning, no pagination
 // for search results
 if (isset($_POST['search_text'])) {
-    $rowsperpage = $numrows;
+	$rowsperpage = $numrows;
 } else {
-    $rowsperpage = 20;
+	$rowsperpage = 20;
 }
 
 $totalpages = ceil($numrows / $rowsperpage);
 // get the current page or set a default
 if (isset($_GET['pg']) && is_numeric($_GET['pg'])) {
-   $currentpage = (int) $_GET['pg'];
+	$currentpage = (int) $_GET['pg'];
 } else {
-   $currentpage = 1;
+	$currentpage = 1;
 }
-if ($currentpage > $totalpages) { $currentpage = $totalpages; }
-if ($currentpage < 1) { $currentpage = 1; }
+if ($currentpage > $totalpages) {
+	$currentpage = $totalpages;
+}
+if ($currentpage < 1) {
+	$currentpage = 1;
+}
 
 $prespagelow = $currentpage * $rowsperpage - $rowsperpage + 1;
 $prespagehigh = $currentpage * $rowsperpage;
-if($prespagehigh > $numrows) { $prespagehigh = $numrows; }
+if ($prespagehigh > $numrows) {
+	$prespagehigh = $numrows;
+}
 
 $offset = ($currentpage - 1) * $rowsperpage;
 $query .= 'LIMIT ' . $offset . ',' . $rowsperpage;
 $result = $db_handle->runQuery($query);
 $content = $db_handle->fetchAssoc($result);
-?><!DOCTYPE HTML>
+?>
+<!DOCTYPE HTML>
 <html>
+
 <head>
 	<title>UKESPS - United Kingdom Education & Skills Placement Services Limited</title>
 	<meta charset="utf-8">
@@ -55,17 +62,18 @@ $content = $db_handle->fetchAssoc($result);
 	<link rel="stylesheet" href="css/font-awesome.css">
 	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="css/styles.css">
-	
+
 	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" />
 	<link rel="stylesheet" href="css/owl.carousel.css">
 	<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen">
 	<!--styles -->
 	<link href="css/select2.css" rel="stylesheet" />
 </head>
+
 <body class="courses-list">
 
 	<!-- page header -->
-	<?php include_once('header.php');?>
+	<?php include_once('header.php'); ?>
 	<!-- content -->
 	<div class="page-content">
 		<div class="container clear-fix">
@@ -75,92 +83,99 @@ $content = $db_handle->fetchAssoc($result);
 					<main>
 						<!-- search -->
 						<div class="category-search">
-							<i class="fa fa-search"></i><!-- 
-						 --><form action="" method="post" class="form-horizontal tasi-form"  enctype="multipart/form-data">
-							 <input name='search_text' type="text" class="input-text" value placeholder="Job Type, e.g Petroleum Sector">
-							<button class="cws-button smaller border-radius alt">Search  Job Sector</button>
+							<i class="fa fa-search"></i>
+							<!-- 
+						 -->
+							<form action="" method="post" class="form-horizontal tasi-form" enctype="multipart/form-data">
+								<input name='search_text' type="text" class="input-text" value placeholder="Job Type, e.g Petroleum Sector">
+								<button class="cws-button smaller border-radius alt">Search Job Sector</button>
 							</form>
 						</div>
-						<h3>Search for <?php echo $_POST['search_text'];?> Sector</h3>
+						<h3>Search for <?php echo $_POST['search_text']; ?> Sector</h3>
 						<!-- / search -->
-						<?php if(isset($content) && !empty($content)) { foreach ($content as $row) { 
-						$jobs_id = $row['jobs_id'];
-						$job_img = $row['job_img'];
-						$job_title = $row['job_title'];
-						$descript = $row['descript'];
-						$location_name = $row['location_name'];
-						$amount_per_period = $row['amount_per_period'];
-						$recruiter_code = $row['recruiter_code'];
-	$recruiter_detail = $recruit_object->get_recruiter_detail($recruiter_code);
-						$recruiter_name = $recruiter_detail['first_name'].' '.$recruiter_detail['last_name'];
-						$recruiter_img = $recruiter_detail['image'];
-				?>
-						<!-- item -->
-						<div class="category-item list clear-fix">
-							<div class="picture">
-								<div class="hover-effect"></div>
-								<div class="link-cont">
-									<a href="job_det?sidi=<?=$jobs_id;?>" class="fancy fa fa-search"></a>
-								</div>
-								<img src="img/jobs/<?=$job_img;?>" data-at2x="img/jobs/<?=$job_img;?>" alt>
-							</div>
-							<h3><a href="job_det?sid=<?=$jobs_id;?>"><?=$job_title;?></a></h3>
-							<div>
-								<div class="star-rating" title="Rated 4.00 out of 5">
-									<span style="width:100%"></span>
-								</div>
-								<div class="count-reviews">( reviews 10 )</div>
-							</div>
-							<p><?=limit_text($descript,25);?></p>
-							<div class="category-info">
-								<span class="price">
-									<span class="amount">
-										<?=$SiteCurrency;?><?=formatMoney($amount_per_period,true);?>
-									</span>
-									<!--<span class="description-price"><?=$amount_per_period;?></span>-->
-								</span>
-								<div class="count-users"><i class="fa fa-location-arrow"></i> <?=$location_name;?></div>
-								<div class="course-lector">
-									<img src="img/recruiter/<?=$recruiter_img;?>" data-at2x="img/recruiter/<?=$recruiter_img;?>" class="avatar" alt>
-									<div class="lector-name">
-										<h4>Posted by</h4>
-										<span><?=$recruiter_name;?></span>
+						<?php if (isset($content) && !empty($content)) {
+							foreach ($content as $row) {
+								$jobs_id = $row['jobs_id'];
+								$job_img = $row['job_img'];
+								$job_title = $row['job_title'];
+								$descript = $row['descript'];
+								$location_name = $row['location_name'];
+								$amount_per_period = $row['amount_per_period'];
+								$recruiter_code = $row['recruiter_code'];
+								$recruiter_detail = $recruit_object->get_recruiter_detail($recruiter_code);
+								$recruiter_name = $recruiter_detail['first_name'] . ' ' . $recruiter_detail['last_name'];
+								$recruiter_img = $recruiter_detail['image'];
+						?>
+								<!-- item -->
+								<div class="category-item list clear-fix">
+									<div class="picture">
+										<div class="hover-effect"></div>
+										<div class="link-cont">
+											<a href="job_det?sidi=<?= $jobs_id; ?>" class="fancy fa fa-search"></a>
+										</div>
+										<img src="img/jobs/<?= $job_img; ?>" data-at2x="img/jobs/<?= $job_img; ?>" alt>
+									</div>
+									<h3><a href="job_det?sid=<?= $jobs_id; ?>"><?= $job_title; ?></a></h3>
+									<div>
+										<div class="star-rating" title="Rated 4.00 out of 5">
+											<span style="width:100%"></span>
+										</div>
+										<div class="count-reviews">( reviews 10 )</div>
+									</div>
+									<p><?= limit_text($descript, 25); ?></p>
+									<div class="category-info">
+										<span class="price">
+											<span class="amount">
+												<?= $SiteCurrency; ?><?= formatMoney($amount_per_period, true); ?>
+											</span>
+											<!--<span class="description-price"><?= $amount_per_period; ?></span>-->
+										</span>
+										<div class="count-users"><i class="fa fa-location-arrow"></i> <?= $location_name; ?></div>
+										<div class="course-lector">
+											<img src="img/recruiter/<?= $recruiter_img; ?>" data-at2x="img/recruiter/<?= $recruiter_img; ?>" class="avatar" alt>
+											<div class="lector-name">
+												<h4>Posted by</h4>
+												<span><?= $recruiter_name; ?></span>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-						<!-- / item -->
-						<?php  }}else{
+								<!-- / item -->
+						<?php  }
+						} else {
 							echo "<h5>No result found</h5>";
 						} ?>
 					</main>
 					<!-- / main content -->
 					<!-- pagination -->
 					<div class="page-pagination clear-fix">
-						<?php 
-							if($currentpage  > 1){?>
-						<a href="?sid=<?=$_GET['sid'];?>&pg=<?=$currentpage-1;?>"><i class="fa fa-angle-double-left"></i></a>
-						<?php } 
-						for($currentpage = $totalpages; $currentpage < $totalpages; $i++): if($currentpage <= $totalpages): ?>
-						<a href="?sid=<?=$_GET['sid'];?>&=<?=$currentpage;?>" class="active"><?=$pg;?>
-						<?php endif; endfor;
-							if ($currentpage > $totalpages) { ?>
-						--><a href="?sid=<?=$_GET['sid'];?>&pg=<?=$currentpage+1;?>"><?=$currentpage+1;?></a><!-- 
-						--><a href="?sid=<?=$_GET['sid'];?>&pg=<?=$currentpage+2;?>"><?=$currentpage+2;?></a><!-- 
-						--><a href="?sid=<?=$_GET['sid'];?>&pg=<?=$currentpage+3;?>"><i class="fa fa-angle-double-right"></i></a>
-						<?php }  ?>
+						<!-- <?php
+									if ($currentpage  > 1) { ?>
+						<a href="?sid=<?= $_GET['sid']; ?>&pg=<?= $currentpage - 1; ?>"><i class="fa fa-angle-double-left"></i></a>
+						<?php }
+									for ($currentpage = $totalpages; $currentpage < $totalpages; $i++) : if ($currentpage <= $totalpages) : ?>
+						<a href="?sid=<?= $_GET['sid']; ?>&=<?= $currentpage; ?>" class="active"><?= $pg; ?>
+						<?php endif;
+									endfor;
+									if ($currentpage > $totalpages) { ?>
+						--><a href="?sid=<?= $_GET['sid']; ?>&pg=<?= $currentpage + 1; ?>"><?= $currentpage + 1; ?></a>
+						<!-- 
+						--><a href="?sid=<?= $_GET['sid']; ?>&pg=<?= $currentpage + 2; ?>"><?= $currentpage + 2; ?></a>
+						<!-- 
+						--><a href="?sid=<?= $_GET['sid']; ?>&pg=<?= $currentpage + 3; ?>"><i class="fa fa-angle-double-right"></i></a>
+					<?php }  ?> -->
 					</div>
 					<!-- / pagination -->
 				</div>
 				<!-- side bar -->
-				<?php include_once('course_sidebar.php');?>
+				<?php include_once('course_sidebar.php'); ?>
 				<!-- / side bar -->
 			</div>
 		</div>
 	</div>
 	<!-- / content -->
 	<!-- footer -->
-	<?php include_once('footer.php');?>
+	<?php include_once('footer.php'); ?>
 	<!-- / footer -->
 	<script src="js/jquery.min.js"></script>
 	<script src="js/select2.js"></script>
@@ -172,14 +187,15 @@ $content = $db_handle->fetchAssoc($result);
 	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
 	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 	<script src="js/jquery.isotope.min.js"></script>
-	
+
 	<script src="js/owl.carousel.min.js"></script>
 	<script src="js/jquery-ui.min.js"></script>
 	<script src="js/jflickrfeed.min.js"></script>
 	<script src="js/jquery.tweet.js"></script>
-	
+
 	<script src="js/jquery.fancybox.pack.js"></script>
 	<script src="js/jquery.fancybox-media.js"></script>
 	<script src="js/retina.min.js"></script>
 </body>
+
 </html>
